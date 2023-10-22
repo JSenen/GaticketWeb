5cfb976cdba43bd2ccaf6b2cdf416b43730f8f53
Juan Senen <95066970+JSenen@users.noreply.github.com>
2023-10-22 18:40:52 +0200

    Update bootstrap 5

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+include "./view/view_header.php";
+?>
+<div class="container">
+  <div class="row justify-content-center align-items-center">
+  <div class="col-auto text-center">
+    <label for="inputPassword6" class="col-form-label">Password</label>
+  </div>
+  <div class="col-auto">
+    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+  </div>
+  <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      Must be 8-20 characters long.
+    </span>
+  </div>
+</div>
+</div>
 
+
+<?php
+include "./view/view_footer.php";
 ?>
