d21ebddfb0c515203822fd91a03572d676af06dc
Juan Senen <juansenen@hotmail.com>
2023-10-26 13:48:08 +0200

    Add JQuery and list incidences user view

--- view/view_header.php
+++ view/view_header.php
@@ -7,6 +7,8 @@
     <!-- Agrega los enlaces a los archivos CSS de Bootstrap -->
   <link href="./resources/css/styles.css" rel="stylesheet" />
   <script src="./resources/js/script.js"></script>
+    <!-- Agrega JQuery -->
+  <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
   </head>
   <body>
   
--- view/view_user.php
+++ view/view_user.php
@@ -9,7 +9,9 @@ $fecha_actual = date('d-m-Y');
         <li class="nav-item">
         <a class="nav-link" href="#"><?php echo $_SESSION['user_tip'] ?></a>
         </li>
-    
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Listado</a>
+        </li>
         <li class="nav-item">
             <a class="nav-link" href="index.php">Salir</a>
         </li>
