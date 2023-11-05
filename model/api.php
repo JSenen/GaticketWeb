d6adf86d1fd31d3370976edfd9c1eb29239a7a1d
Juan Senen <95066970+JSenen@users.noreply.github.com>
2023-11-05 20:54:25 +0100

    Count device by type

--- model/api.php
+++ model/api.php
@@ -821,4 +821,33 @@ function eraseDevice($idDevice){
 
     curl_close($ch);
 }
+
+// ======== DISPOSITIVOS POR TIPO ==========
+function getAllByType($typeId){
+
+    //Obtenemos nombre del tipo de dispositivo
+    $url = BASE_URL.'types/'.$typeId;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $url = curl_exec($ch);
+    curl_close($ch);
+
+    //Recopila los datos 
+    $result = json_decode($url, true);
+    $name = $result['typeName'];
+
+    //Buscamos los dispositivos con el tipo.
+    $urldevice = BASE_URL.'device?typeName='.$name;
+    $ch = curl_init($urldevice);
+    curl_setopt($ch, CURLOPT_URL, $urldevice);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $url = curl_exec($ch);
+    curl_close($ch);
+    //Recopila los datos 
+    $devicesTypeName = json_decode($url, true);
+    return $devicesTypeName;
+
+
+}
 ?>
